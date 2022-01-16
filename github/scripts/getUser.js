9afdc3d9feb607a431523e2b34136ddc17101045
nakul177 <96129513+nakul177@users.noreply.github.com>
2022-01-16 10:21:37 +0530

    strat

---
+++ github/scripts/getUser.js
@@ -0,0 +1,13 @@
+async function getUser(user){
+    try{
+        let response = await fetch(`https://api.github.com/users/${user}`
+        )
+        let data= await response.json()
+        return data
+    }
+    catch(err){
+        console.log(err);
+    }
+  
+}
+export default getUser
